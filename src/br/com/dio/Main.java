15bd221fd60f851ec17c84d6b1bb0576f8299e47
LucasTAlmeida <49998772+LucasTAlmeida@users.noreply.github.com>
2025-08-03 13:24:26 -0300

    Update Main.java

--- src/br/com/dio/Main.java
+++ src/br/com/dio/Main.java
@@ -31,12 +31,12 @@ public class Main {
         var option = -1;
         while (true){
             System.out.println("Selecione uma das opções a seguir");
-            System.out.println("1 - Iniciar um novo Jogo");
-            System.out.println("2 - Colocar um novo número");
-            System.out.println("3 - Remover um número");
+            System.out.println("1 - Iniciar novo Jogo");
+            System.out.println("2 - Colocar novo número");
+            System.out.println("3 - Remover número");
             System.out.println("4 - Visualizar jogo atual");
             System.out.println("5 - Verificar status do jogo");
-            System.out.println("6 - limpar jogo");
+            System.out.println("6 - Limpar jogo");
             System.out.println("7 - Finalizar jogo");
             System.out.println("8 - Sair");
 
